5c2099d5c020e8695ebe417e3aa4fc0a1bf93217
Osvaldo Nicolas Barbery Merida <barchos123@gmail.com>
2022-06-18 21:04:02 -0400

    Added the Parser and all their necessary methods

--- shell.py
+++ shell.py
@@ -2,7 +2,9 @@ import cat
 
 while True:
     text = input('Cat > ')
-    result, error = cat.run('<stdin>',text)
+    result, error = cat.run('<stdin>', text)
 
-    if error: print(error.as_string())
-    else: print(result)
+    if error:
+        print(error.as_string())
+    else:
+        print(result)
