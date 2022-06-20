12fe5ce22405b99e15cecb4ae38f57961ae6f373
vanebustillos <vanebustillos1@gmail.com>
2022-06-20 13:19:06 -0400

    Revert "Merge pull request #1 from NicoBarbery/branch1"
    This reverts commit de405b92db3f41777f0d6676872a2d5982df1ad0, reversing
    changes made to 22c89c4eba036488166e04eb2ba04318259db134.

--- shell.py
+++ shell.py
@@ -1,36 +1,8 @@
+import cat
 
-
-#RUN
-from interpreter import Context, Interpreter
-from lexer import Lexer
-from parserLR import Parser
-
-
-
-def run(fn, text):
-    #Generate tokens
-    lexer = Lexer(fn, text)
-    tokens, error = lexer.make_tokens()
-    print('\nTOKENS: \n',tokens,'\n')
-    if error: return None, error
-
-    #Generate AST
-    parser = Parser(tokens)
-    ast = parser.parse()
-    if ast.error: return None, ast.error
-
-    #Run program
-    interpreter = Interpreter()
-    context = Context('<program>')
-    result = interpreter.visit(ast.node, context)
-
-    return result.value, result.error
-
-
-#main
 while True:
     text = input('Cat > ')
-    result, error = run('<stdin>', text)
+    result, error = cat.run('<stdin>', text)
 
     if error:
         print(error.as_string())
